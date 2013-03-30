93c17336c13ba2b3829cfd93897d785b2327b12a
Barbie <barbie@missbarbell.co.uk>
2013-03-30 08:03:22 +0000

    upped version

--- lib/Labyrinth/Plugin/Articles/Sections.pm
+++ lib/Labyrinth/Plugin/Articles/Sections.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 
 use vars qw($VERSION $ALLSQL $SECTIONID);
-$VERSION = '5.10';
+$VERSION = '5.11';
 
 =head1 NAME
 
@@ -170,7 +170,7 @@ Miss Barbell Productions, L<http://www.missbarbell.co.uk/>
 
 =head1 COPYRIGHT & LICENSE
 
-  Copyright (C) 2002-2012 Barbie for Miss Barbell Productions
+  Copyright (C) 2002-2013 Barbie for Miss Barbell Productions
   All Rights Reserved.
 
   This module is free software; you can redistribute it and/or
