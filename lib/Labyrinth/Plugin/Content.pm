dab9faa8f684c18060e8ad0b8537ea57de05c595
Barbie <barbie@missbarbell.co.uk>
2014-02-02 15:48:14 +0000

    upped version

--- lib/Labyrinth/Plugin/Content.pm
+++ lib/Labyrinth/Plugin/Content.pm
@@ -3,7 +3,7 @@ package Labyrinth::Plugin::Content;
 use strict;
 use warnings;
 
-my $VERSION = '5.13';
+my $VERSION = '5.14';
 
 =head1 NAME
 
@@ -138,7 +138,7 @@ Miss Barbell Productions, L<http://www.missbarbell.co.uk/>
 
 =head1 COPYRIGHT & LICENSE
 
-  Copyright (C) 2002-2013 Barbie for Miss Barbell Productions
+  Copyright (C) 2002-2014 Barbie for Miss Barbell Productions
   All Rights Reserved.
 
   This module is free software; you can redistribute it and/or
--- lib/Labyrinth/Plugin/Core.pm
+++ lib/Labyrinth/Plugin/Core.pm
@@ -3,7 +3,7 @@ package Labyrinth::Plugin::Core;
 use warnings;
 use strict;
 
-our $VERSION = '5.13';
+our $VERSION = '5.14';
 
 1;
 
@@ -67,7 +67,7 @@ Miss Barbell Productions, L<http://www.missbarbell.co.uk/>
 
 =head1 COPYRIGHT & LICENSE
 
-  Copyright (C) 2002-2013 Barbie for Miss Barbell Productions
+  Copyright (C) 2002-2014 Barbie for Miss Barbell Productions
   All Rights Reserved.
 
   This module is free software; you can redistribute it and/or
